eb8b6a54b4c5ef546f3e77e592a348b0ad3a44c2
EfrainBatista <efrainbatista13@outlook.com>
2024-10-21 21:04:29 -0500

    conectar con el formulario

--- src/app/heroes/page/new-page/new-page.component.ts
+++ src/app/heroes/page/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,9 +8,24 @@ import { Component } from '@angular/core';
   styles: ``,
 })
 export class NewPageComponent {
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl<string>('', {nonNullable: true}), // Ejemplo de tipado
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   // quemado, se debe buscar al backend
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' }, // desc es la descripción
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+  onSubmit():void {
+    
+  }
 }
